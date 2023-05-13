5da6d92a85396f294beec79160a31f6a852825b2
Hussein Guevara <80933892+HusseinGuevara@users.noreply.github.com>
2023-05-12 22:56:19 -0700

    Add files via upload

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,5 @@
 import './../../assets/css/homeStyles.css';
 import Quantum from '../../assets/images/quantum.png'
-import Button from '@mui/joy/Button';
 import Divider from '@mui/joy/Divider';
 
 
@@ -12,16 +11,16 @@ const Home = () => {
             <div className='top-container'>
             </div>
             <div className='title-container'>
-                <img src={Quantum}/>
+                <img src={Quantum} alt='Logo'/>
                 <h2>QUANTUM AI</h2>
             </div>
             <div className='header-container'>
               
             </div>
             <div className='button-container'>
-                <Button type='submit' variant='outlined' fullWidth > Log in </Button>
+                <a href='/login' >Log in</a>
                 <Divider orientation="horizontal">or</Divider>
-                <Button type='submit' variant='outlined' fullWidth > Sign up </Button>
+                <a href='/' >Sign up</a>
             </div>
             <footer className='footer-container'>
                 <p> © 2023 Quantum AI Inc.</p>
