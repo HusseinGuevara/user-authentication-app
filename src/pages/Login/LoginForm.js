21034f053bd2341660926197fe91fa3560f670ca
Hussein Guevara <80933892+HusseinGuevara@users.noreply.github.com>
2023-05-17 20:36:16 -0700

    Add files via upload

--- src/pages/Login/LoginForm.js
+++ src/pages/Login/LoginForm.js
@@ -2,10 +2,12 @@ import Input from '@mui/joy/Input';
 import Button from '@mui/joy/Button';
 import EmailIcon from '@mui/icons-material/Email';
 import KeyIcon from '@mui/icons-material/Key';
+import Divider from '@mui/joy/Divider';
 import './../../assets/css/formStyles.css';
 
 const LoginForm = () => {
     return(
+        
         <section>
             <div className='top-container'></div>
             <div className='top-header-container'>
@@ -19,6 +21,20 @@ const LoginForm = () => {
                         <a href='/forgotPassword' >Forgot password?</a>
                         <p>Don't have an account? <a href='/signup' >Sign up</a></p>
                     </div> 
+                    <Divider orientation="horizontal">or</Divider>
+                    <div id="g_id_onload"
+                        data-client_id="YOUR_GOOGLE_CLIENT_ID"
+                        data-login_uri="https://your.domain/your_login_endpoint"
+                        data-auto_prompt="false">
+                    </div>
+                    <div className="g_id_signin"
+                        data-type="standard"
+                        data-size="large"
+                        data-theme="filled_black"
+                        data-text="sign_in_with"
+                        data-shape="pill"
+                        data-logo_alignment="left">
+                    </div>
                 </div>    
                 <footer className='footer-container'>
                     <p> © 2023 Quantum AI Inc.</p>
--- src/pages/Sign Up/SignUpForm.js
+++ src/pages/Sign Up/SignUpForm.js
@@ -4,6 +4,7 @@ import EmailIcon from '@mui/icons-material/Email';
 import KeyIcon from '@mui/icons-material/Key';
 import PersonIcon from '@mui/icons-material/Person';
 import PasswordIcon from '@mui/icons-material/Password';
+import Divider from '@mui/joy/Divider';
 import './../../assets/css/formStyles.css';
 
 const SignUpForm = () => {
@@ -22,6 +23,20 @@ const SignUpForm = () => {
                     <Button type='submit' variant='outlined' fullWidth >Sign up </Button>
                     <p>Already have an account? <a href='/login' >Sign up</a></p>
                 </div> 
+                <Divider orientation="horizontal">or</Divider>
+                    <div id="g_id_onload"
+                        data-client_id="YOUR_GOOGLE_CLIENT_ID"
+                        data-login_uri="https://your.domain/your_login_endpoint"
+                        data-auto_prompt="false">
+                    </div>
+                    <div className="g_id_signin"
+                        data-type="standard"
+                        data-size="large"
+                        data-theme="filled_black"
+                        data-text="sign_in_with"
+                        data-shape="pill"
+                        data-logo_alignment="left">
+                    </div>
             </div>
             <footer className='footer-container'>
                 <p> © 2023 Quantum AI Inc.</p>
