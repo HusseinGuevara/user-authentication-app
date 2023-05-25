f8766d4c503adbd943c511d067e60b669541b003
Hussein Guevara <80933892+HusseinGuevara@users.noreply.github.com>
2023-05-24 18:41:13 -0700

    Add files via upload

--- src/pages/Login/LoginForm.js
+++ src/pages/Login/LoginForm.js
@@ -2,7 +2,7 @@ import Input from '@mui/joy/Input';
 import Button from '@mui/joy/Button';
 import EmailIcon from '@mui/icons-material/Email';
 import KeyIcon from '@mui/icons-material/Key';
-import Divider from '@mui/joy/Divider';
+// import Divider from '@mui/joy/Divider';
 import './../../assets/css/formStyles.css';
 
 const LoginForm = () => {
@@ -21,7 +21,7 @@ const LoginForm = () => {
                         <a href='/forgotPassword' >Forgot password?</a>
                         <p>Don't have an account? <a href='/signup' >Sign up</a></p>
                     </div> 
-                    <Divider orientation="horizontal">or</Divider>
+                    {/* <Divider orientation="horizontal">or</Divider>
                     <div id="g_id_onload"
                         data-client_id="YOUR_GOOGLE_CLIENT_ID"
                         data-login_uri="https://your.domain/your_login_endpoint"
@@ -34,7 +34,7 @@ const LoginForm = () => {
                         data-text="sign_in_with"
                         data-shape="pill"
                         data-logo_alignment="left">
-                    </div>
+                    </div> */}
                 </div>    
                 <footer className='footer-container'>
                     <p> © 2023 Quantum AI Inc.</p>
--- src/pages/Sign Up/SignUpForm.js
+++ src/pages/Sign Up/SignUpForm.js
@@ -4,7 +4,7 @@ import EmailIcon from '@mui/icons-material/Email';
 import KeyIcon from '@mui/icons-material/Key';
 import PersonIcon from '@mui/icons-material/Person';
 import PasswordIcon from '@mui/icons-material/Password';
-import Divider from '@mui/joy/Divider';
+// import Divider from '@mui/joy/Divider';
 import './../../assets/css/formStyles.css';
 
 const SignUpForm = () => {
@@ -21,9 +21,9 @@ const SignUpForm = () => {
                     <Input  startDecorator={<KeyIcon />} placeholder='Password' fullWidth />
                     <Input startDecorator={< PasswordIcon />} placeholder='Confiirm Password' fullWidth />
                     <Button type='submit' variant='outlined' fullWidth >Sign up </Button>
-                    <p>Already have an account? <a href='/login' >Sign up</a></p>
+                    <p>Already have an account? <a href='/login' >Log in</a></p>
                 </div> 
-                <Divider orientation="horizontal">or</Divider>
+                {/* <Divider orientation="horizontal">or</Divider>
                     <div id="g_id_onload"
                         data-client_id="YOUR_GOOGLE_CLIENT_ID"
                         data-login_uri="https://your.domain/your_login_endpoint"
@@ -36,7 +36,7 @@ const SignUpForm = () => {
                         data-text="sign_in_with"
                         data-shape="pill"
                         data-logo_alignment="left">
-                    </div>
+                    </div> */}
             </div>
             <footer className='footer-container'>
                 <p> © 2023 Quantum AI Inc.</p>
